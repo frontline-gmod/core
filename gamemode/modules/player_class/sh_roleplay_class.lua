425581d624be5a4d38ad6d7b11826cb6ae01dd92
MeaDolphi <meadolphi@protonmail.com>
2020-02-01 15:15:00 +0500

    Создание папки с классами и добавление ролеплей класса

---
+++ gamemode/modules/player_class/sh_roleplay_class.lua
@@ -0,0 +1,25 @@
+local PLAYER = {}
+
+PLAYER.DisplayName			= "Roleplay Player Class"
+
+PLAYER.WalkSpeed			= 100
+PLAYER.RunSpeed				= 225
+PLAYER.CrouchedWalkSpeed	= 15
+PLAYER.DuckSpeed			= 0.3
+PLAYER.UnDuckSpeed			= 0.3
+PLAYER.JumpPower			= 200
+PLAYER.CanUseFlashlight		= true
+PLAYER.MaxHealth			= 100
+PLAYER.StartHealth			= 100
+PLAYER.StartArmor			= 0
+PLAYER.DropWeaponOnDie		= false
+PLAYER.TeammateNoCollide	= true
+PLAYER.AvoidPlayers			= true
+PLAYER.UseVMHands			= true
+
+function PLAYER:GetHandsModel()
+	local playermodel = player_manager.TranslateToPlayerModelName( self.Player:GetModel() )
+	return player_manager.TranslatePlayerHands( playermodel )
+end
+
+player_manager.RegisterClass("roleplay_class", PLAYER, "player_default")
