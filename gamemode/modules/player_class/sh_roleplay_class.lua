831b13412052431e3166807f41703b2c12bf6465
MeaDolphi <meadolphi@protonmail.com>
2020-02-01 18:53:11 +0500

    Исправление табуляции и исправление включения фонарика

--- gamemode/modules/player_class/sh_roleplay_class.lua
+++ gamemode/modules/player_class/sh_roleplay_class.lua
@@ -2,20 +2,20 @@ local PLAYER = {}
 
 PLAYER.DisplayName			= "Roleplay Player Class"
 
-PLAYER.WalkSpeed			= 100
-PLAYER.RunSpeed				= 225
+PLAYER.WalkSpeed	= 100
+PLAYER.RunSpeed	= 225
 PLAYER.CrouchedWalkSpeed	= 15
-PLAYER.DuckSpeed			= 0.3
-PLAYER.UnDuckSpeed			= 0.3
-PLAYER.JumpPower			= 200
-PLAYER.CanUseFlashlight		= true
-PLAYER.MaxHealth			= 100
-PLAYER.StartHealth			= 100
-PLAYER.StartArmor			= 0
-PLAYER.DropWeaponOnDie		= false
+PLAYER.DuckSpeed	= 0.3
+PLAYER.UnDuckSpeed	= 0.3
+PLAYER.JumpPower	= 200
+PLAYER.AllowFlashlight	=	true
+PLAYER.MaxHealth	= 100
+PLAYER.StartHealth	= 100
+PLAYER.StartArmor	= 0
+PLAYER.DropWeaponOnDie	= false
 PLAYER.TeammateNoCollide	= true
-PLAYER.AvoidPlayers			= true
-PLAYER.UseVMHands			= true
+PLAYER.AvoidPlayers	= true
+PLAYER.UseVMHands	= true
 
 function PLAYER:GetHandsModel()
 	local playermodel = player_manager.TranslateToPlayerModelName( self.Player:GetModel() )
