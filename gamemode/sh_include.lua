4f33e7ee6887145bc947c2c765041ad1378f5402
MeaDolphi <meadolphi@protonmail.com>
2020-02-01 19:49:34 +0500

    Убрал комментарии

--- gamemode/sh_include.lua
+++ gamemode/sh_include.lua
@@ -6,26 +6,22 @@ flrp.include_sh = function(f)
 	flrp.include_cl(f)
 end
 
--- Includes a file from the prefix.
 function flrp. (fileName, strState)
-	-- Only include server-side if we're on the server.
+
 	if ((strState == "server" or string.find(fileName, "sv_")) and SERVER) then
 		flrp.include_sv(fileName)
-	-- Shared is included by both server and client.
+
 	elseif (strState == "shared" or string.find(fileName, "sh_")) then
 		flrp.include_sh(fileName)
-	-- File is sent to client, included on client.
+
 	elseif (strState == "client" or string.find(fileName, "cl_")) then
 		flrp.include_cl(fileName)
 	end
 end
 
-
--- Include files based off the prefix within a directory.
 function flrp.include_dir(directory)
 	local fol = GM.FolderName .. '/gamemode/' .. directory .. '/'
 
-	-- Find all of the files within the directory.
 	for k, v in ipairs(file.Find(fol .. '*', 'LUA')) do
 		local fileName = directory..'/'..v
 		flrp.include_load(fileName)
@@ -42,7 +38,7 @@ end
 
 local libs_isload
 
-if !libs_isload then -- include libraries
+if !libs_isload then
 	flrp.include_sh 'libs/pon.lua'
 	flrp.include_sh 'libs/netstream.lua'
 
