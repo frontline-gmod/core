bf06e16d50d83f4aa9080c4d0e294ceaa524be3a
MeaDolphi <meadolphi@protonmail.com>
2020-02-08 01:26:04 +0500

    Фикс

--- gamemode/core/cl_core.lua
+++ gamemode/core/cl_core.lua
@@ -1,11 +1,11 @@
 if flrp.config.enable_secondary_modules["QMenuAdmin"] == true then
   hook.Add( "OnSpawnMenuOpen", "CheckPermissionForSpawnMenu", function()
-    if flrp.config.usergroup["" .. LocalPlayer():GetUserGroup() .. ""] then return true else return false end
+    if !flrp.config.usergroup["" .. LocalPlayer():GetUserGroup() .. ""] then return false end
   end )
 end
 
 if flrp.config.enable_secondary_modules["ContextAdmin"] == true then
   hook.Add( "OnContextMenuOpen", "CheckPermissionForContextMenu", function()
-    if flrp.config.usergroup["" .. LocalPlayer():GetUserGroup() .. ""] then return true else return false end
+    if !flrp.config.usergroup["" .. LocalPlayer():GetUserGroup() .. ""] then return false end
   end )
 end
