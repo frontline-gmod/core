897dec4c4c976fbe0c4fc76545c9d74735c8c2cc
Gor Mkhitaryan <gor.mhitaryan@list.ru>
2020-02-08 22:01:27 +0300

    Создание и получение персонажа из базы

---
+++ gamemode/modules/database/sv_hooks.lua
@@ -0,0 +1,34 @@
+hook.Add("PlayerInitialSpawn", "frontline_player_initialze", function (ply)
+  print("[FL]Player spawned");
+
+  local existsQuery = DataBase:prepare("SELECT * FROM users WHERE steamid64 = ?")
+
+  function existsQuery:onSuccess(data)
+	   if(#data == 0) then
+        local insertQuery = DataBase:prepare("INSERT INTO users (name, steamid64, team, unit, rank) VALUES (?,?,?,?,?)")
+
+        function insertQuery:onSuccess()
+            print("[FL-RP] Персонаж создан")
+        end
+
+        function insertQuery:onError(err)
+            print("[FL ERROR] An error occured while executing the query: " .. err)
+        end
+
+        insertQuery:setString(1, ply:Name());
+        insertQuery:setNumber(2, ply:SteamID64());
+        insertQuery:setString(3, "TEAM_DOB");
+        insertQuery:setString(4, "Доброволец");
+        insertQuery:setString(5, "");
+
+        insertQuery:start();
+     end
+  end
+
+  function existsQuery:onError(err)
+  	print("[FL ERROR] An error occured while executing the query: " .. err)
+  end
+
+  existsQuery:setNumber(1, ply:SteamID64());
+  existsQuery:start();
+end)
